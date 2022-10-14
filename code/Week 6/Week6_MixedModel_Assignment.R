61ce8feadaeed6db9fdf2ba0b50c7e5475f46379
Matt Wilson <wilsonmatt@susqu.edu>
2022-10-14 12:55:22 -0400

    Wk6 graded

--- code/Week 6/Week6_MixedModel_Assignment.R
+++ code/Week 6/Week6_MixedModel_Assignment.R
@@ -17,6 +17,8 @@ r.squaredGLMM(glmm.mod.add)
 glmm.mod.inter <- glmmPQL(activity.level ~ claw.width, family = binomial, random = ~ 1 | block, data = df)
 summary(glmm.mod.inter)
 r.squaredGLMM(glmm.mod.inter)
+
+#These are setup right but only include one or two predictor variables in each - supposed to be three in each so activity~temp+cue+claw or activity~temp*cue+claw 
 # The authors used proportional consumption of prey as the (y) in their model, but did not include this in the dataset.
   # So we are going to create it - run the following line, assuming df= your data frame (feel free to change that):
 df$prop.cons <- df$eaten/df$prey
@@ -27,17 +29,19 @@ summary(glmm.coms)
 r.squaredGLMM(glmm.coms)
 # (Q1) - The code in line 8 is performing two operations at once. What are they? (2 pts)
 #  The code is signifying random effects in the data and specifying the family distribution as
-# binomial
+# binomial #This question was about the "df$prop.cons <- df$eaten/df$prey" line. Yes..I know it was 13 and not 8.
 
 # (Q2) - Did the interactive effect change which variables predict proportional consumption? How, specifically, did the results change? (5 pts)
 # Yes, when running the models with prop.cons as the y temperature has an unsignificant p value while toadfish.cue.treatmentYES has a significant p value
-# while the claw.width has a p value of 0. Which is most likely an error? I am not sure.
+# while the claw.width has a p value of 0. Which is most likely an error? I am not sure. 
+#This particular model formula output rounds the p-value so it is probably just very close to zero.
 
 # (Q3) - Plot the residuals of both models. Do you think either model is a good fit? Why or why not? (3 pts)
 plot(glmm.coms.t$residuals)
 plot(glmm.coms$residuals)
 # the glmm.coms or the plot comparing proportional comsumption with claw width has a tighter pattern in the plot. 
 # But both are spread out and fuzzy, they do not provide good fits.
+#But why are they bad fits? Remember patterns=bad in residuals.
 # Re-run both models as generalized additive models instead (using gam). Then compare the AIC of both models. (4 points each)
 gam.coms.t <- gam(prop.cons ~ temperature + toadfish.cue.treatment, family = binomial, random = list(block=~ 1), data = df)
 gam.coms <- gam(prop.cons ~ claw.width*carapace.width, family = binomial, random = ~ 1 | block, data = df)
@@ -50,8 +54,9 @@ AIC(gam.coms.t, gam.coms)
 plot(gam.coms.t$residuals)
 plot(gam.coms$residuals)
 # I am not confident in either :D Both show really spread data with absolutely no pattern.
+  #I would say there actually is a little bit of a pattern...which is no buenos.
 
 
 # https://youtu.be/dQw4w9WgXcQ 
-
+#I love it. Thank you.
 
