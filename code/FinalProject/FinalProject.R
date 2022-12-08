5ace7f1a1acf9e7759c5d1f06b10de1f33b1c68e
NSherinsky <Sherinsky@susqu.edu>
2022-12-08 09:52:26 -0500

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- code/FinalProject/FinalProject.R
+++ code/FinalProject/FinalProject.R
@@ -38,15 +38,47 @@ head(macroinvert4)
 
 
 eighteenturb3$Date <- format(as.POSIXct(eighteenturb3$dateTime,format='%Y-%m-%d %H:%M:%S'),format='%m/%d/%Y')
-head(eighteenturb4)                       
+head(eighteenturb3)    
 
-?aggregate
+oakorc3$Date <- format(as.POSIXct(oakorc3$dateTime,format='%Y-%m-%d %H:%M:%S'),format='%m/%d/%Y')
+head(oakorc3)
+
+install.packages("stringr")
+library(stringr)
+
+eighteenturb3$Water.Body <- str_replace(eighteenturb3$Water.Body, "04219768", "Eighteenmile Creek")
+head(eighteenturb3)
+
+oakorc3$Water.Body <- str_replace(oakorc3$Water.Body, "0422016550", "Oak Orchard Creek")
+head(oakorc3)
+
+
 macro.mean <- aggregate(macroinvert4$Diversity.Score ~ macroinvert4$Water.Body + macroinvert4$Date.Sampled, FUN = "mean")
 head(macro.mean)
 
-eight.mean <- aggregate(eighteenturb3$turbidity ~eighteenturb3$dateTime, FUN = "mean")
+eight.mean <- aggregate(eighteenturb3$turbidity ~eighteenturb3$Date + eighteenturb3$Water.Body, FUN = "mean")
 head(eight.mean)
 
-#week 6 for gam
+oak.mean <- aggregate(oakorc3$turbidity ~oakorc3$Date + oakorc3$Water.Body, FUN = "mean")
+head(oak.mean)
 
+?merge
+colnames(oak.mean) <- c("Date","Water.Body","turbidity")
+
+colnames(eight.mean) <- c("Date","Water.Body","turbidity")
+ 
+colnames(macro.mean) <- c("Water.Body","Date","Diversity.score")
+
+macro.mean$Date <- format(as.POSIXct(macro.mean$Date,format='%m/%d/%Y'),format='%m/%d/%Y')
+
+Creeks <- rbind(oak.mean,eight.mean)
+
+
+macro.creeks <- merge(macro.mean,Creeks, by = "Date")
+
+library(mgcv)
+#week 6 for gam
+gam.mod1 <- gam(Flight.initiation.distance..FID.~Object + Area, family = Gamma, random = list(ID=~ 1), data = df)
+
+gam.mod1 <- gam(macro.creeks~ Date + Water.Body, family = Gamma, random = list(ID=~1))
 
