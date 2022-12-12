2399fa520d2e86bd08b0f8ea351f662a11fda51c
NSherinsky <Sherinsky@susqu.edu>
2022-12-12 17:48:03 -0500

    Done >.<

--- code/FinalProject/FinalProject.R
+++ code/FinalProject/FinalProject.R
@@ -10,6 +10,7 @@ library(dataRetrieval)
 ?readNWISuv
 #turbidity data code 63680
 
+#Gathering the data needed
 setwd("C:/GitHub/Sherinskynl/code/FinalProject")
 macroinvert <- read.csv("EighteenmileCreek.csv")
 eighteenturb <- readNWISuv("04219768","63680","2021-08-03","2021-08-04")
@@ -17,6 +18,7 @@ head(eighteenturb)
 oakorc <- readNWISuv("0422016550","63680", "2021-08-04","2021-08-05")
 head(oakorc)
 
+#Formatting the data tables to make mean gathering more direct
 eighteenturb2 <- eighteenturb[,-1]
 eighteenturb3 <- eighteenturb2[,-4:-5]
 head(eighteenturb3)
@@ -36,7 +38,7 @@ head(macroinvert3)
 macroinvert4 <- macroinvert3[,-4:-6]
 head(macroinvert4)
 
-
+#Renaming values for ease of merging
 eighteenturb3$Date <- format(as.POSIXct(eighteenturb3$dateTime,format='%Y-%m-%d %H:%M:%S'),format='%m/%d/%Y')
 head(eighteenturb3)    
 
@@ -52,7 +54,7 @@ head(eighteenturb3)
 oakorc3$Water.Body <- str_replace(oakorc3$Water.Body, "0422016550", "Oak Orchard Creek")
 head(oakorc3)
 
-
+#means to have the same number of columns
 macro.mean <- aggregate(macroinvert4$Diversity.Score ~ macroinvert4$Water.Body + macroinvert4$Date.Sampled, FUN = "mean")
 head(macro.mean)
 
@@ -62,6 +64,7 @@ head(eight.mean)
 oak.mean <- aggregate(oakorc3$turbidity ~oakorc3$Date + oakorc3$Water.Body, FUN = "mean")
 head(oak.mean)
 
+#merging the three data sets
 ?merge
 colnames(oak.mean) <- c("Date","Water.Body","turbidity")
 
@@ -76,20 +79,31 @@ Creeks <- rbind(oak.mean,eight.mean)
 
 macro.creeks <- merge(macro.mean,Creeks, by = c("Date","Water.Body"))
 
+#data analysis
 library(mgcv)
-install.packages("MASS")
-library(MASS)
 ?mgcv
 ?gam
 
+gam.mod1 <- gam(Diversity.score~ Water.Body, family = Gamma, random = list(ID=~1), data = macro.creeks)
+AIC(gam.mod1)
+gam.mod2 <- gam(Diversity.score~ turbidity, family = Gamma, random = list(ID=~1), data = macro.creeks)
+AIC(gam.mod2)
 gam.diverse <- gam(Diversity.score~ turbidity + Water.Body, family = Gamma, random = list(ID=~1), data = macro.creeks)
 summary(gam.diverse)
 
-plot(gam.diverse$residuals, ylim = c(-.1,.1))
+plot(gam.diverse$residuals, ylim = c(-.1,.1), ylab = "GAM Residuals", main = "Residuals for Generalized Additive Model")
 vis.gam(gam.diverse, view=c("turbidity","Water.Body"), theta = 45, color = "heat")
 AIC(gam.diverse)
+AIC(gam.mod1,gam.mod2,gam.diverse)
 
-gam.mod2 <- gam(Diversity.score~ turbidity*Water.Body, family = Gamma, random = list(ID=~1), data = macro.creeks)
-summary(gam.mod2)
-AIC(gam.mod2)
+
+
+#Plotting the data
+macro.creeks$Labels <- c("EighteenMile8.3.21","EighteenMile8.4.21","OakOrchard8.4.21","OakOrchard8.5.21")
+plot(macro.creeks$Diversity.score, macro.creeks$turbidity, xlim = c(0,10),xlab = "Diversity Score",ylab = "Turbidity", main = "Comparing Diversity and Turbidity in Creeks in New York")
+text(macro.creeks$Diversity.score, macro.creeks$turbidity, labels = macro.creeks$Labels)
+
+boxplot(Diversity.score*turbidity ~ Water.Body, data = macro.creeks, ylim = c(0,35), xlab = "Water Body", ylab = "Diversity Score and Turbidity", main = "Divsersity combinded with Trubidity")
+
+
 
